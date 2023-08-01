08600cc9d373396d025aa580da2347e46a336b8a
Chi Le <lehuechi04@gmail.com>
2023-08-01 16:04:23 +0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # calculator
 The Odin Project - 5th project
+
+A simple calculator.
+
+## Usage
+
+[Live Demo]()
+
+## Credits
+
+Font: Barbie Medium.
+
+## Progress
+
+In progress.
+
---
+++ game.js
@@ -0,0 +1,96 @@
+let num1;
+let num2;
+let operator;
+let result;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return 'Dummy~';
+  }
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  if (operator == '+') return add(num1, num2);
+  else if (operator === '-') return subtract(num1, num2);
+  else if (operator === '*') return multiply(num1, num2);
+  else return divide(num1, num2);
+}
+
+function clear() {
+  curDisplay.textContent = '0';
+  afterDisplay.textContent = '';
+  num1 = 0;
+  num2 = 0;
+  result = 0;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  clear();
+});
+
+const curDisplay = document.querySelector('.cur-display');
+const afterDisplay = document.querySelector('.after-display');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => {
+  if (button.className === 'digit') {
+    if (curDisplay.textContent === '0' && afterDisplay.textContent === '') { // Clear the default 0 in current display
+      curDisplay.textContent = '';
+    }
+    if (result == curDisplay.textContent && (afterDisplay.textContent === num1.toString()+operator+num2.toString() || afterDisplay.textContent === result)) { // Start a new calculation
+      clear();
+      curDisplay.textContent = '';
+    }
+    curDisplay.textContent += button.textContent;
+  }
+  else if (button.className === 'operator') {
+    if (afterDisplay.textContent === num1.toString() + operator) { // Press operator button before getting result
+      if (result === 'Dummy~') {
+        clear();
+      }
+      else {
+        num2 = parseInt(curDisplay.textContent);
+        result = operate(operator, num1, num2);
+        curDisplay.textContent = result;
+      }
+    }
+    
+    num1 = parseInt(curDisplay.textContent);
+    operator = button.textContent;
+    afterDisplay.textContent = curDisplay.textContent + operator;
+    curDisplay.textContent = ''; 
+
+  }
+  else if (button.className === 'equal') {
+    if (afterDisplay.textContent === '') { // Press equal after putting a number only
+      result = curDisplay.textContent;
+      afterDisplay.textContent = curDisplay.textContent;
+    }
+    else if (afterDisplay.textContent === num1.toString()+operator+num2.toString() || afterDisplay.textContent === curDisplay.textContent) {} //Press equal after pressing equal
+    else {
+      if (afterDisplay.textContent === '-') {  // Press minus operator first then a number
+        num1 = 0;
+      }
+      num2 = parseInt(curDisplay.textContent);
+      result = operate(operator, num1, num2);
+      afterDisplay.textContent += curDisplay.textContent; 
+      curDisplay.textContent = result;
+    }
+  }
+  else {
+    clear();
+  }
+}))
+  
